849bde62f9a2f415a61cf96359797e17f3806277
Aleyna Mandacı <aleynamandaci@gmail.com>
2022-07-12 22:50:12 +0300

    add ApiHelper file

---
+++ ChargerApp/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  ChargerApp
+//
+//  Created by Aleyna Mandacı on 11.07.2022.
+//
+
+struct User: Decodable {
+  var email: String?
+  var token: String?
+  var userID: String?
+}
--- ChargerApp/ViewController.swift
+++ ChargerApp/ViewController.swift
@@ -11,6 +11,7 @@ import UserNotifications
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager: CLLocationManager?
+    @IBOutlet weak var emailField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager?.delegate = self
         locationManager?.requestAlwaysAuthorization()
 
-       
     }
 
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -31,7 +31,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
         }
     }
-
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        //postRequest()
+        postRequest(url: "http://ec2-18-197-100-203.eu-central-1.compute.amazonaws.com:8080/auth/login", parameters: ["email": emailField.text!, "deviceUDID": String(UIDevice.current.identifierForVendor!.uuidString)], token: nil)
+    }
+    
+    
+    
 
 }
 
