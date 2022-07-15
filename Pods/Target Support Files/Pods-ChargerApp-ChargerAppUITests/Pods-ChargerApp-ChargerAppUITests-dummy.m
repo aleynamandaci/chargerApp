e2c03490a00294b141bfac4cf4cdcd0606c676cb
Aleyna Mandacı <aleynamandaci@gmail.com>
2022-07-15 16:35:40 +0300

    Alamofire

---
+++ Pods/Target Support Files/Pods-ChargerApp-ChargerAppUITests/Pods-ChargerApp-ChargerAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChargerApp_ChargerAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ChargerApp_ChargerAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-ChargerApp/Pods-ChargerApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChargerApp : NSObject
+@end
+@implementation PodsDummy_Pods_ChargerApp
+@end
